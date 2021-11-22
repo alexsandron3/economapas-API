d4523a1e33864bb1c1901f2491347e149b896bd4
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2021-11-22 18:51:29 -0300

    Update head.php

--- config/head.php
+++ config/head.php
@@ -1,5 +1,5 @@
 <?php 
-  require '..\vendor\autoload.php';
-  // $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
-  // $dotenv->load();
+  require  __DIR__.'../../vendor/autoload.php';  
+  $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+  $dotenv->load();
   // require_once(__DIR__.'.\helpers.php');

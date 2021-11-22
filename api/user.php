8a48a3333f5e870608598e6f62f4325e51292556
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2021-11-22 20:21:22 -0300

    altera lista de métodos permitidos via CORS

--- api/user.php
+++ api/user.php
@@ -2,7 +2,7 @@
   require_once('../config/head.php');
   require_once(__DIR__.'/classes/Database.php');
   header('Access-Control-Allow-Headers: access');
-  header('Access-Control-Allow-Methods: GET, POST, UPDATE');
+  header('Access-Control-Allow-Methods: POST');
   header('Content-Type: application/json; charset=UTF-8');
   header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
   header('Access-Control-Allow-Origin: *');

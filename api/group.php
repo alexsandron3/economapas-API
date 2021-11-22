8c308c6ed3bd624fabc35dd5a681ed49641494ce
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2021-11-22 18:43:27 -0300

    Update group.php

--- api/group.php
+++ api/group.php
@@ -1,6 +1,6 @@
 <?php
-  require_once('../config/head.php');
-  require_once(__DIR__.'/classes/Database.php');
+  // require_once('../config/head.php');
+  // require_once(__DIR__.'/classes/Database.php');
   // header('Access-Control-Allow-Headers: access');
   // header('Access-Control-Allow-Methods: GET, POST, UPDATE');
   // header('Content-Type: application/json; charset=UTF-8');

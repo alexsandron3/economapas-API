4ca9b7c47d25fb970c10301d07f927b2604a919d
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2021-11-22 16:43:09 -0300

    Cria condições para outros tipos de requisição e API para listar grupos

--- api/group.php
+++ api/group.php
@@ -11,40 +11,81 @@
   $conn = $dbConnection->dbConnection();
   $data = json_decode(file_get_contents("php://input"));
   $returnData = [];
-  // return print_r(json_encode(IsNullOrEmptyString($data->groupName)));
+  // return print_r(json_encode($data->groupName));
   
   if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retirando espaços em branco
-    $groupName = $data->groupName;
-    $selectedCities = json_encode($data->selectedCities);
-    $createdBy = $data->userId;
+    // Verificando se os dados estão sendo enviados
+    if(!isset($data->groupName) || 
+    !isset($data->selectedCities) || 
+    !isset($data->userId)) {
+      $returnData = msg(0, 422, 'Por favor, preencha os campos corretamente!');
+    }else {
+  
+      $groupName = $data->groupName;
+      $selectedCities = json_encode($data->selectedCities);
+      $createdBy = $data->userId;
 
-    // Preparando query
-    $addGroup = "INSERT INTO economapas.citygroup (groupName, selectedCities, createdAt, createdBy) VALUES (:groupName, :selectedCities, NOW(), :createdBy)";
-    $stmt = $conn->prepare($addGroup);
-    try {
-      //code...
-      $stmt->bindValue(':groupName', $groupName, PDO::PARAM_STR);
-      $stmt->bindValue(':selectedCities', $selectedCities, PDO::PARAM_STR);
-      $stmt->bindValue(':createdBy', $createdBy, PDO::PARAM_STR);
-      if($stmt->execute()) {
-        $returnData = [
-          "success" => 1,
-          "message" => "Grupo criado com sucesso!",
-          "grupo" => $conn->lastInsertId(),
-        ];
-      }else {
-        $returnData = [
-          "success" => 0,
-          "message" => "Houve um erro!",
-        ];
+      // Preparando query
+      $addGroup = "INSERT INTO economapas.citygroup (groupName, selectedCities, createdAt, createdBy) VALUES (:groupName, :selectedCities, NOW(), :createdBy)";
+      $stmt = $conn->prepare($addGroup);
+      try {
+        //code...
+        $stmt->bindValue(':groupName', $groupName, PDO::PARAM_STR);
+        $stmt->bindValue(':selectedCities', $selectedCities, PDO::PARAM_STR);
+        $stmt->bindValue(':createdBy', $createdBy, PDO::PARAM_STR);
+        if($stmt->execute()) {
+          $returnData = [
+            "success" => 1,
+            "message" => "Grupo criado com sucesso!",
+            "grupo" => $conn->lastInsertId(),
+          ];
+        }else {
+          $returnData = [
+            "success" => 0,
+            "message" => "Houve um erro!",
+          ];
+        }
+      } catch (\Throwable $error) {
+        $returnData = msg(0,500,$error->getMessage());
+        
       }
-    } catch (\Throwable $error) {
-      $returnData = msg(0,500,$error->getMessage());
-      
-    }
+  }
+  }elseif($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $fetchGroups = "SELECT * FROM economapas.cityGroup";
+    $stmt = $conn->prepare($fetchGroups);
+    try {
+      $stmt->execute();
+      if($stmt->rowCount()){
+          $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          $returnData = [
+            "success" => 1,
+            "message" => 'Pesquisa realizada com sucesso!',
+            "cliente" => $row
+          ];
+        }else {
+          $returnData = msg(0, 422, 'Passeio não encontrado!');
+        }
+      } catch (\Throwable $error) {
+        $returnData = msg(0,500,$error->getMessage());
+  
+      }
+  }elseif($_SERVER['REQUEST_METHOD'] === 'UPDATE') {
+
+  }elseif($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
   }else {
     $returnData = msg(0, 405, 'Inválid Method');
   }
 
   echo json_encode($returnData);
+
+
+  // {
+//     "groupName": "",
+//     "userId": "1",
+//     "selectedCities": [
+//         "Manaus - AM",
+//         "Macapá - AP",
+//         "Maceió - AL"
+//     ]
+// }

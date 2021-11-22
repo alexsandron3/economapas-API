f3868173c50596a35214aca7991049b6e26c3944
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2021-11-22 18:54:49 -0300

    '

--- api/user.php
+++ api/user.php
@@ -1,5 +1,6 @@
 <?php
   require_once('../config/head.php');
+  require_once('../config/helpers.php');
   require_once(__DIR__.'/classes/Database.php');
   header('Access-Control-Allow-Headers: access');
   header('Access-Control-Allow-Methods: GET, POST, UPDATE');

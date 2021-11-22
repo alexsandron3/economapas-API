b0a42fd1cd00ab3ff882c2173c52002a572cc601
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2021-11-22 18:41:35 -0300

    Update group.php

--- api/group.php
+++ api/group.php
@@ -1,11 +1,11 @@
 <?php
   require_once('../config/head.php');
   require_once(__DIR__.'/classes/Database.php');
-  header('Access-Control-Allow-Headers: access');
-  header('Access-Control-Allow-Methods: GET, POST, UPDATE');
-  header('Content-Type: application/json; charset=UTF-8');
-  header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
-  header('Access-Control-Allow-Origin: *');
+  // header('Access-Control-Allow-Headers: access');
+  // header('Access-Control-Allow-Methods: GET, POST, UPDATE');
+  // header('Content-Type: application/json; charset=UTF-8');
+  // header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+  // header('Access-Control-Allow-Origin: *');
   
   $dbConnection = new Database();
   $conn = $dbConnection->dbConnection();
